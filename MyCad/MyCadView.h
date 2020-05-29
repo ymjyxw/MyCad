33821da7334d87ab36985c42a5002262cafea22f
PeiXinHuang <1716973911@qq.com>
2020-05-29 09:14:02 +0800

    创建MFC项目

---
+++ MyCad/MyCadView.h
@@ -0,0 +1,49 @@
+﻿
+// MyCadView.h: CMyCadView 类的接口
+//
+
+#pragma once
+
+
+class CMyCadView : public CView
+{
+protected: // 仅从序列化创建
+	CMyCadView() noexcept;
+	DECLARE_DYNCREATE(CMyCadView)
+
+// 特性
+public:
+	CMyCadDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CMyCadView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // MyCadView.cpp 中的调试版本
+inline CMyCadDoc* CMyCadView::GetDocument() const
+   { return reinterpret_cast<CMyCadDoc*>(m_pDocument); }
+#endif
+
